0307be085fa29eac7521f4364ca0d35f6b0feca2
Stewart Smith <stewartlsmith12@gmail.com>
2014-02-19 09:55:03 -0500

    modify secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,6 +10,7 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 require 'securerandom'
+
 def secure_token
     token_file = Rails.root.join('.secret')
     if File.exist?(token_file)
